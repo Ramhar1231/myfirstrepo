6870cb467d59ea1549c896d5554764376760c03f
mustafa <mustafa@mail.com>
2026-08-29 05:20:20 +0000

    app data added

---
+++ app.java
@@ -0,0 +1,8 @@
+// The class name must exactly match the filename (e.g., Main.java)
+public class Main {
+    public static void main(String[] args) {
+        // Prints the text inside quotes to the console with a new line
+        System.out.println("Hello, World!");
+    }
+}
+
